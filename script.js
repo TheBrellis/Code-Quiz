e7a6cd35b863406848609f55339ff898260ce29d
TheBrellis <brianroyalellis@gmail.com>
2019-10-15 17:33:02 -0400

    starting working on the startQuestions function. working on a way to get create the button elements to generate in relation to the number of possible choices.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var time = document.querySelector('#timer');
 var start = document.querySelector('#startQuiz');
 var homeScreen = document.querySelector('#homeScreen');
 
+
 //Global variables
 var questions = [
     {
@@ -21,30 +22,55 @@ var timeTotal = questions.length * 15;
 
 //Functions ----------------------------------------------------------------------------------------
 
-function timerStart (){
-time.textContent = timeTotal;
-var timerInterval = setInterval( function(){
-    if (timeTotal === 0){
-      return;
-    } else {
-    timeTotal--;
+function timerStart() {
     time.textContent = timeTotal;
+    var timerInterval = setInterval(function () {
+        if (timeTotal === 0) {
+            return;
+        } else {
+            timeTotal--;
+            time.textContent = timeTotal;
+        }
     }
-}
-, 100);
+        , 1000);
 }
 
-function clearStart () { //method for removing node found on https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+function clearStart() { //method for removing node found on https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
     var wipe = homeScreen.cloneNode(false); //creates an idential node that is false (blank)
     homeScreen.parentNode.replaceChild(wipe, homeScreen); // swaps the origional node for the blank node
 };
 
+function startQuestions() {
+    var prompts = document.querySelector('#prompts');
+    var title = document.createElement('h2');
+    prompts.appendChild(title);
+
+    for (var i = 0; i < questions.length; i++) { // Looping through all of the questions to build the correct number of buttons and storing them with the corect index
+
+        var x = document.createElement('button');
+        //breaks here, need a way to creat a button element and append it to the title, resulting in multiple buttons. 
+        // could possibly do this by adding the buttons to an unordered list? Would just need to build a list with an ID in the html...but would that be the same as this? Just with different elements? Could build list first and then add elements to it?
+        prompts.title.appendChild(x);
+        var z = prompts.title.x;
+        z.setAttribute('class', 'buttons');
+        z.setAttribute('data-index', [i]);
+    }
+    console.log(x);
+    var feedback = document.createElement('p');
+    document.body.appendChild(feedback);
+}
+
+//USE DATASET OBJECT WHEN ADDING VALUES TO BUTTONS
 
 
+//assign every possible answer an index, and then randomize the index so that they 'never' appear in the same order twice. Possibly build the indeces before and then reassign them using random numbers.
 
-//Event Listeners   ----------------------------------------------------------
-start.addEventListener('click', function(){
+//Series of User Events ----------------------------------------------------------
+
+start.addEventListener('click', function () {
     timerStart(); //method for calling multiple functions with a single event listener found on https://stackoverflow.com/questions/25028853/addeventlistener-two-functions
     clearStart();
-  //  nextQuestion();
+    startQuestions();
+    //  nextQuestion();
 });
+//correctAnswer.addEventListener('click', function())
