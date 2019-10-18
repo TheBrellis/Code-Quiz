3b368560c4fbae8c7826f71dcbbef5916840d576
TheBrellis <brianroyalellis@gmail.com>
2019-10-18 19:54:33 -0400

    - updated nextQuestion() to include the title of the current question - created click event for all of the button with data-index choice - click event calls for the answer to be checked (checkAnswer() created) then moves on to the next questions

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ var start = document.querySelector('#startQuiz');
 var homeScreen = document.querySelector('#homeScreen');
 var choicesDiv = document.querySelector('#choices');
 
+// pretty sure this query call is broken-------------
+var qButtons = document.querySelectorAll('{data-type: choice}')
+var qTitle = document.querySelector('#qTitle');
+var qFeedback = document.querySelector('#feedback');
+
 //Global variables
 var timeTotal = questions.length * 15; //total time the quiz will run for
 var qCurrent = 0; // current question the quiz is referencing
+
 //Functions ----------------------------------------------------------------------------------------
 
 function timerStart() {
@@ -31,21 +37,43 @@ function clearStart() {
 function nextQuestion() {
 
     var numChoices = questions[qCurrent].choices.length;  //adding a variable to determine number of buttons needed for answers, could loop this for varying number of answers. Would require this function be recalled for every question. 
+    
     for (var i = 0; i < numChoices ; i++) { // Looping through all of the questions to build the correct number of buttons and storing them with the corect index
-        var btns = document.createElement('buttons');
-        choicesDiv.appendChild(btns);
-        btns.setAttribute('data-index', i);
-        btns.setAttribute('data-type', 'choice');
-        btns.setAttribute('type','submit')
-        btns.setAttribute('class','col-md-12')
-        btns.textContent = questions[qCurrent].choices[i];
+        var qChoices = document.createElement('buttons');
+        choicesDiv.appendChild(qChoices);
+        qChoices.setAttribute('data-index', i); //may not need this?
+        qChoices.setAttribute('data-type', 'choice');
+        qChoices.setAttribute('type','submit')
+        qChoices.setAttribute('class','col-md-12')
+        qChoices.textContent = questions[qCurrent].choices[i];
     }
+    qTitle.textContent = questions[qCurrent].title;
 
-}
+};
+
+function checkAnswer() {
+    var feedbackInterval = setInterval(function(event) {
+        // if the value of the target that is clicked equals the value presented in the answer key
+        if (event.target.value === question[qCurrent].answer.value){
+            qFeedback.textContent = 'Correct!'; //change text content of feedback to correct
+            return;
+        }
+        qFeedback.textContent = 'False you fail!';
+        timeTotal = timeTotal - 15;
+
+        }, 3000);
+
+        return timeTotal;
+};
 
 //USE DATASET OBJECT WHEN ADDING VALUES TO BUTTONS
 
+/*Commit notes
+- updated nextQuestion() to include the title of the current question
+- created click event for all of the button with data-index 'choice'
+- click event calls for the answer to be checked (checkAnswer() created) then moves on to the next questions
 
+*/
 
 
 //Series of User Events ----------------------------------------------------------
@@ -57,3 +85,8 @@ start.addEventListener('click', function () {
 });
 
 //assign an event for clicking any buttons, checks values, then gives feedback (for breif amount of time) and applys awards/penalties to counter, clears choicesDiv, then runs the nextQuestion
+qButtons.addEventListener('click', function(event) {
+    checkAnswer();
+    nextQuestion();
+});
+
