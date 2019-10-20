7274bdf8d1c36c72b71f960d9ac7a3570150f2d8
TheBrellis <brianroyalellis@gmail.com>
2019-10-20 18:41:51 -0400

    added link to imbd credits page in credits.m

--- audioFiles/credits.m
+++ audioFiles/credits.m
@@ -1,3 +1,4 @@
-## Audio Files from tv show The Office (US)
-# files sourced from soundboard.com
-# audiotrimmer.com used to modify audio files
+# Audio Files from tv show The Office (US)
+### files sourced from soundboard.com
+### audiotrimmer.com used to modify audio files
+### https://www.imdb.com/title/tt0386676/companycredits?ref_=ttrel_sa_3
