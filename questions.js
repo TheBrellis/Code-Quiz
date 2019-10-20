3f4cac047e94c1a46df4f73079bfc0e60b604091
TheBrellis <brianroyalellis@gmail.com>
2019-10-20 18:38:41 -0400

    added additional questions to questions.js. Future possible improvment would be adding a large number of questions and using a randomizer to select a set of 5-10 to use in each iteration of the quiz.

--- questions.js
+++ questions.js
@@ -6,7 +6,27 @@
     },
     {
         title: 'The condition in an if / else statment is enclosed within ____.',
-        choices: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
+        choices: ['quotes', 'parenthesis', 'curly brackets', 'square brackets'],
         answer: 'parenthesis'
+    },
+    {
+        title: 'Which HTML tag is used to identify JavaScript code?',
+        choices: ['<javascript>','<script>','<scripted>','<js>'],
+        answer: '<script>'
+    },
+    {
+        title: 'Which line below would display "Hello World" in a pop-up window on the browser?',
+        choices: ['alertbox("Hello World");', 'msg("Hello World");', 'msgbox("Hello World");', 'alert("Hello World");'],
+        answer: 'alert("Hello World");'
+    },
+    {
+        title: 'True/False: The first <style> tag in the <html> script takes precedense over all <style> tags? ',
+        choices: ['True','False'],
+        answer: 'False'
+    },
+    {
+        title: 'Which built-in method removes the last element from an array and returns that element?',
+        choices: ['.last()','.get()','.pop()','None of the above'],
+        answer: '.pop()'
     }
 ]
