015c32b1d0ed34b3d681cffd88910ac556cfec96
TheBrellis <brianroyalellis@gmail.com>
2019-10-19 20:24:29 -0400

    added function quizComplete() that builds a final screen prompting user with score and option to enter name + store name and number in localStorage

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ var choicesDiv = document.querySelector('#choices');
 var qPrompts = document.querySelector('#prompts');
 var qTitle = document.querySelector('#qTitle');
 var qFeedback = document.querySelector('#feedback');
-
+var results = document.querySelector('#results');
 //Global variables
-var timeTotal = questions.length * 15; //total time the quiz will run for
+var timeTotal = 5// questions.length * 15; //total time the quiz will run for
 var qCurrent = 0; // current question the quiz is referencing
 //Functions ----------------------------------------------------------------------------------------
 
@@ -22,11 +22,11 @@ function timerStart() {
             time.textContent = timeTotal;
         }
     }
-        , 1000);
+        , 100);
 }
 
 function nextQuestion() {
-    qPrompts.setAttribute('class', 'container jumbotron my-4 py-2')
+    qPrompts.setAttribute('class', 'container jumbotron col-md-8 my-4 py-2')
     // removes buttons from previous question
     choicesDiv.innerHTML = '';
     var numChoices = questions[qCurrent].choices.length;  
@@ -35,12 +35,13 @@ function nextQuestion() {
         var qChoices = document.createElement('buttons');
         choicesDiv.appendChild(qChoices);
         qChoices.setAttribute('data-type', 'choice');
-        qChoices.setAttribute('type','submit')
-        qChoices.setAttribute('class','col-md-6 btn-primary text-center m-2 p-2 ')
+        qChoices.setAttribute('type','submit');
+        qChoices.setAttribute('class','col-md-6 d-block text-center btn-primary rounded m-2 p-2');
         qChoices.textContent = questions[qCurrent].choices[i];
     }
     qTitle.textContent = questions[qCurrent].title;
-
+  //  updated eventListener in nextQuestion function to checkAnswer(), iterate qCurrent, and run nextQuestio
+    //n. checkAnswer now gives feedback for 1 seconds and then disappears. 
     //Adding Event Listeners for Buttons within the nextQuestion function (buttons don't exist until this function is run)
    qButtons = document.querySelectorAll('[data-type]');
    qButtons.forEach(choice => {
@@ -66,6 +67,25 @@ function checkAnswer() {
         qFeedback.textContent = '';
     },1000);
 };
+
+function quizComplete(){
+
+    choicesDiv.innerHTML = ''; //removing buttons that may exist
+    qTitle.textConent = 'Quiz Complete!'
+
+    var scoreReport = document.createElement('p');
+    choicesDiv.appendChild(scoreReport);
+    scoreReport.textContent = 'Your Score is: ' + timeTotal + '!';
+
+    var initials = document.createElement('form',{id: 'initials', class: 'col-md-6'});
+    initials.textContent = 'Your Name Here:'
+    var submitScore = document.createElement('button',{type: 'submit', id: 'submitScore', class: 'btn-success p-2 col-md-3 rounded'});
+    submitScore.textConent = 'Submit Score!';
+    results.appendChil(initials);
+    results.appendChild(submitScore);
+
+}
+    
 
 //USE DATASET OBJECT WHEN ADDING VALUES TO BUTTONS
 
