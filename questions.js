b19f2e72ab93ee41d26436286b4862d91c112f1c
TheBrellis <brianroyalellis@gmail.com>
2020-05-07 18:36:12 -0400

    fixed some typos

--- questions.js
+++ questions.js
@@ -1,17 +1,17 @@
- var questions = [
+var questions = [
     {
         title: 'Commonly used data types DO NOT include:',
         choices: ['strings', 'booleans', 'alerts', 'numbers'],
         answer: 'alerts'
     },
     {
-        title: 'The condition in an if / else statment is enclosed within ____.',
+        title: 'The condition in an if / else statement is enclosed within ____.',
         choices: ['quotes', 'parenthesis', 'curly brackets', 'square brackets'],
         answer: 'parenthesis'
     },
     {
         title: 'Which HTML tag is used to identify JavaScript code?',
-        choices: ['<javascript>','<script>','<scripted>','<js>'],
+        choices: ['<javascript>', '<script>', '<scripted>', '<js>'],
         answer: '<script>'
     },
     {
@@ -21,12 +21,12 @@
     },
     {
         title: 'True/False: The first <style> tag in the <html> script takes precedense over all <style> tags? ',
-        choices: ['True','False'],
+        choices: ['True', 'False'],
         answer: 'False'
     },
     {
         title: 'Which built-in method removes the last element from an array and returns that element?',
-        choices: ['.last()','.get()','.pop()','None of the above'],
+        choices: ['.last()', '.get()', '.pop()', 'None of the above'],
         answer: '.pop()'
     }
 ]
